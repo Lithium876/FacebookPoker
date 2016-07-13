3895ff239acd923ad6415cd490d5c092d4fa09b2
Lomar Lilly <lithiumlilly3@gmail.com>
2016-07-13 14:02:02 -0600

    Create FBPoker.py

---
+++ FBPoker.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time,sys
+
+browser = webdriver.PhantomJS("C:\Program Files (x86)\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\\bin\phantomjs.exe")
+browser.set_window_size(1024, 768)
+
+def poke():
+	browser.get("https://www.facebook.com/pokes")
+	try:
+		a = browser.find_elements_by_xpath("""//*[@class="_42ft _4jy0 _4jy3 _4jy1 selected _51sy"]""")
+		for i in range(0,len(a)):
+			pokeback = a[i].text
+			if pokeback == "Poke Back":
+				a[i].click()
+			else:
+				break
+		poke()
+	except:
+		time.sleep(1)
+		poke()
+
+def login(user,pwd):
+	browser.get("https://www.facebook.com/")
+	browser.find_element_by_id("email").send_keys(user + Keys.TAB)
+	browser.find_element_by_id("pass").send_keys(pwd + Keys.RETURN)
+	time.sleep(1)
+	poke()
+
+def main(arg):
+ if len(arg)<3:
+  sys.exit("script should be called with your username and password")
+ user=arg[1]
+ pwd=arg[2]
+ data=login(user,pwd)
+
+if __name__=="__main__":
+ main(sys.argv)
